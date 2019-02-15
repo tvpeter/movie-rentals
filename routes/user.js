d9eef223dd04ea95a0e09db0189ebac039d51422
tvpeter <withtvpeter@gmail.com>
2019-02-15 12:37:32 +0100

    add route for users

---
+++ routes/user.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const {User, validate} = require('../models/user');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+
+router.post('/', async(req, res)=> {
+    //validate the request
+   const {error} = validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //check if user already exits
+    let user = await User.findOne({email: req.body.email});
+    if(user) return res.status(400).send('User already registered');
+
+    //create and save user
+    //  user = new User({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // });
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password =await bcrypt.hash(user.password, salt);
+
+     await user.save();
+
+    // return res.send({
+    //     name: user.name,
+    //     email: user.email
+    // });
+    return res.send(_.pick(user, ['id','name', 'email']))
+
+});
+
+router.get('/', async(req, res)=>{
+    const users = await User.find().sort('name');
+
+    return res.send(users);
+});
+
+module.exports = router;
