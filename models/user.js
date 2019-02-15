467c4e889b75c82f4312a7ab500de650c96bde73
tvpeter <withtvpeter@gmail.com>
2019-02-15 12:33:28 +0100

    add user schema

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 40
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        minlength: 6,
+        maxlength: 50
+
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength:4,
+        maxlength: 1024
+
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+//we can use joi.password-complexity package to validate the strength of supplied passwords
+
+function validateUser(user){
+    const schema = {
+        name: Joi.string().min(2).max(40).required(),
+        email: Joi.string().required().email(),
+        password: Joi.string().min(4).max(50).required()
+    }
+    return Joi.validate(user, schema);
+}
+exports.User = User;
+exports.validate = validateUser;
