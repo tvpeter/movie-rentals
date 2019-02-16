694fbb2ae5f20c8403b803c0ed6a462f3e61c0ec
tvpeter <withtvpeter@gmail.com>
2019-02-16 14:10:40 +0100

    add user authentication

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/user');
+const auth = require('./routes/auth');
 
 mongoose.connect('mongodb://localhost/movies')
 .then(()=> console.log(`Connected to Mongodb..`))
@@ -21,6 +22,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 app.use('/', index);
 
 app.listen(port, ()=>console.log(`Listening on port ${port}`));
---
+++ routes/auth.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const {User} = require('../models/user');
+const _ = require('lodash');
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+
+router.post('/', async(req, res)=> {
+    //validate the request
+   const {error} = validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //check if user already exits
+    let user = await User.findOne({email: req.body.email});
+    if(!user) return res.status(400).send('Invalid email/password');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) return res.status(400).send('Invalid email/password');
+
+    return res.send(true);
+});
+
+router.get('/', async(req, res)=>{
+    const users = await User.find().sort('name');
+
+    return res.send(users);
+});
+
+
+function validate(req){
+    const schema = {
+        email: Joi.string().required().email(),
+        password: Joi.string().min(4).max(50).required()
+    }
+    return Joi.validate(req, schema);
+}
+
+module.exports = router;
