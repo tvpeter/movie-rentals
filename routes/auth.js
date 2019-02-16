02815824510637306f3746c10b16714b2dfe1873
tvpeter <withtvpeter@gmail.com>
2019-02-16 15:45:33 +0100

    Generate jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const {User} = require('../models/user');
 const _ = require('lodash');
 const Joi = require('joi');
@@ -17,7 +18,8 @@ router.post('/', async(req, res)=> {
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if(!validPassword) return res.status(400).send('Invalid email/password');
 
-    return res.send(true);
+    const token = jwt.sign({_id: user._id}, 'jwtPrivateKey');
+    return res.send(token);
 });
 
 router.get('/', async(req, res)=>{
