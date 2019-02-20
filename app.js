da3cdbd945e42f1c2cfca6ab969a4248c0bd7539
tvpeter <withtvpeter@gmail.com>
2019-02-20 11:33:42 +0100

    extract validation from index

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
+const winston = require('winston');
 const express = require('express');
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
 const app = express();
 require('./startup/routes')(app);
-
 require('./startup/logger')();
 require('./startup/database')();
-require('./startup/logger')();
 require('./startup/config')();
-
+require('./startup/validation')();
 
 const port = process.env.PORT || 4000;
-
-
-app.listen(port, ()=>console.log(`Listening on port ${port}`));
+app.listen(port, ()=>winston.info(`Listening on port ${port}`));
